55dd381fa7ff3d35005163d663199dd64ca036ae
Matt MP <themp731@gmail.com>
2016-09-04 17:04:48 -0400

    Pushing the work for my assignment

---
+++ README.md
@@ -0,0 +1,10 @@
+## This is the Cleaning Data Set Peer Review Exercise
+
+This takes the information from a Samsung Galaxy S Smartphone and tries to categorize the results based on the the activity information. 
+
+The main script here is the run_analysis.r which will take the data that has been downloaded into the local directory or repository and clean it, then write a new dataset that has the averages for each subject and each ‘activity.’
+
+### FROM UCI
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data. 
+
+The sensor signals (accelerometer and gyroscope) were pre-processed by applying noise filters and then sampled in fixed-width sliding windows of 2.56 sec and 50% overlap (128 readings/window). The sensor acceleration signal, which has gravitational and body motion components, was separated using a Butterworth low-pass filter into body acceleration and gravity. The gravitational force is assumed to have only low frequency components, therefore a filter with 0.3 Hz cutoff frequency was used. From each window, a vector of features was obtained by calculating variables from the time and frequency domain.
---
+++ Run_Analysis.R
@@ -0,0 +1,60 @@
+"This Script Assumes Data Has Been Downloaded To A Local File"
+
+#Loading Libraries
+library(data.table)
+library(dplyr)
+library(quantmod)
+
+"PART 1: Merge training and test sets"
+#loading the data sets
+trainset <- read.table("./Data/test/X_test.txt")
+testset <- read.table("./Data/train/X_train.txt")
+
+#Merge the data sets using rBind
+merged <- rbind(trainset, testset)
+
+#Give column names
+columns <- read.table("./Data/features.txt")
+colnames(merged) <- t(columns[,2])
+
+"PART 2: Get mean and std-dev for each measurement"
+#using regular expressions we look for the column names
+# that have 'std' or 'mean' in their titles
+# then populate a new table with only those columns
+
+means <- grep("mean()", colnames(merged), fixed = TRUE)
+std_devs <- grep("std()", colnames(merged), fixed = TRUE)
+measures <- merged[, c(means, std_devs)]
+
+"Part 3 Getting Activity Labels"
+#Reading in labels
+act_labels <-  read.table("./data/activity_labels.txt")
+
+#Combing Test/Trains for the results
+test_y <- read.table("./data/test/y_test.txt")
+train_y <-read.table("./data/train/y_train.txt")
+merged_y <- rbind(test_y, train_y)
+
+#Putting all the info together
+results <- cbind(merged_y, measures)
+colnames(results)[1] <- "Activity"
+
+"Part 5, labeling the activities"
+act_labels$V2 <-  as.character(act_labels$V2)
+for(i in 1:length(results[,1])){
+  results[i,1] <- act_labels[results[i,1],1]
+}
+
+"Part 6, create a tidy data set with averages"
+# Read in the subjects info
+sub_test <- read.table("./data/test/subject_test.txt")
+sub_train <- read.table("./data/train/subject_train.txt")
+
+#Combines the Subject Info
+sub_total <- rbind(sub_test, sub_train)
+all_data <- cbind(sub_total, results)
+colnames(all_data)[1] <- "Subject"
+
+#Gets Aggregate Data
+tidy_dataset <- aggregate(all_data, by = list(all_data$Subject, all_data$Activity), mean)
+write.table(tidy_dataset, file="tidy_data.txt")
